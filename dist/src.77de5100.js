88a2170e45f622d97961109b5cb20693000f2d90
SwingDasDing <bradcrastintor@gmail.com>
2021-07-31 17:48:25 +0100

    x

--- dist/src.77de5100.js
+++ dist/src.77de5100.js
@@ -1248,7 +1248,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53594" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54485" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
